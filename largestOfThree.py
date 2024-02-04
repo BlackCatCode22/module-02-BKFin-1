e2e8e12d18361cc90e76db028aaa3660a33ceb73
Brian Finley <bkfinley1@my.scccd.edu>
2024-02-04 11:55:33 -0800

    Cleaning up largestOfThree.py

--- largestOfThree.py
+++ largestOfThree.py
@@ -1,11 +1,6 @@
 def doCompare(first, second, third) :
-
-# Using Ternary operator (cleaner)
-#    largest = (first if (first > second and first > third) else
-#               (second if (second > first and second > third) else third))
-#    return largest
-    
-# Using Nested if statements
+  
+    # Using Nested if statements
     if first > second:
         if first > third :
             largest = first
@@ -22,7 +17,8 @@ def doCompare(first, second, third) :
 first = float(input("Enter First Number: "))
 second = float(input("Enter Second Number: "))
 third = float(input("Enter third Number: "))
-# Running the doCompare function, passing the inputs as parameters
+# Running the doCompare function, passing the inputs as parameters, saving the returned value into
+# the largest variable
 largest = doCompare(first, second, third)
 # Got the returned largest result, and sending it to output
 print("The largest number of those three is:", largest)
