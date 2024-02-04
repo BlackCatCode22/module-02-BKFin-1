82b26e22cb9ba2856ffc1c6832a7efca66f8c2aa
Brian Finley <bkfinley1@my.scccd.edu>
2024-02-04 12:14:29 -0800

    Added error-trapping with try-except

--- largestOfThree.py
+++ largestOfThree.py
@@ -14,9 +14,32 @@ def doCompare(first, second, third) :
     return largest
 
 # Have to convert/cast the input to either integer or float or it won't work properly
-first = float(input("Enter First Number: "))
-second = float(input("Enter Second Number: "))
-third = float(input("Enter third Number: "))
+# Also error-trapping using a while loop and a try-except block for each input.
+
+while True:
+    try:
+        first = float(input("Enter First Number: "))
+        break
+    except ValueError:
+        print("You must enter a numerical value!")
+        continue
+
+while True:
+    try:
+        second = float(input("Enter Second Number: "))
+        break
+    except ValueError:
+        print("You must enter a numerical value!")
+        continue
+
+while True:
+    try: 
+        third = float(input("Enter third Number: "))
+        break
+    except ValueError:
+        print("You must enter a numerical value!")
+        continue
+
 # Running the doCompare function, passing the inputs as parameters, saving the returned value into
 # the largest variable
 largest = doCompare(first, second, third)
